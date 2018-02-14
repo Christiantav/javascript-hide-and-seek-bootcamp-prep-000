534142d4ae549ec88d2b22a06db4546e0eb8bf36
Christian Tavares <chrisadrianotav@gmail.com>
2018-02-14 09:11:46 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ return document.querySelector('#nested .target');
 }
 
 function increaseRankBy(n) {
-var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+var lis = document.querySelectorAll('#app ul.ranked-list li')
 for(let i = 0; i < lis.length; i++) {
   lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
 }
